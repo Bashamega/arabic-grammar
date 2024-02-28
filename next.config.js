acaabcd2274fdb53b9726e2b0598f0307a596785
Bashamega <adambashaahmednaji@gmail.com>
2024-02-28 16:23:00 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn-arabic-grammar.vercel.app'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/2/4/3/index.tsx
+++ src/pages/2/4/3/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import 'react-tabs/style/react-tabs.css';
+import Image from "next/image";
 export default function Names(){
     return(
         <div className="lg:w-[700px]">
@@ -26,29 +27,11 @@ export default function Names(){
                 <p className="text-[#527853] text-lg">البد هو تابع يوضح المبدل منه، ويتبعه في الإعراب</p>
                 <br />
                 <div className="text-[#527853] text-lg text-right">
-                <table className="border border-[#EE7214] text-[#527853] border-collapse w-full text-center text-lg" dir="rtl">
-                    <tr className=" text-2xl bg-[#EE7214]">
-                        <td className="border border-[#527853]">كل من كل (مطابق)</td>
-                        <td className="border border-[#527853]">بعض من كل</td>
-                        <td className="border border-[#527853]">اشتمال</td>
-                    </tr>
-                    
-                    <tr>
-                        <td className="border border-[#EE7214]">البدل = المبدل منه</td>
-                        <td className="border border-[#EE7214]">البدل &gt; المبدل منه البدل جزء من المبدل منه</td>
-                        <td className="border border-[#EE7214]">المبدل منه يشتمل على البدل وليس البدل جزءا ولا مطابقا</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-[#EE7214]">لا يحتاج إلى ضمير يعود على المبدل منه</td>
-                        <td className="border border-[#EE7214]">يحتاج إلى ضمير يعود على المبدل منه</td>
-                        <td className="border border-[#EE7214]">يحتاج إلى ضمير يعود على المبدل منه</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-[#EE7214]">قابل الفروق عمر</td>
-                        <td className="border border-[#EE7214]">سقط المنزل سقفه</td>
-                        <td className="border border-[#EE7214]">أحببت السيارة سرعتها</td>
-                    </tr>
-                </table>
+                <div className="flex items-center w-full justify-center">
+                    <Link href="https://cdn-arabic-grammar.vercel.app/files/media/Presentation1.jpg" target="_blank" rel="noreferrer">
+                        <Image src={"https://cdn-arabic-grammar.vercel.app/files/media/Presentation1.jpg"} width={400} className=" tex" height={0} alt="presentation 1"></Image>
+                    </Link>
+                </div>
                 <br></br>
                 <div className=" rounded text-right bg-slate-200 mt-2 p-2 border-1" dir="rtl">
                     <h3 className=" text-2xl">ملاحظة:</h3>
