a558214de47ef275fc419fd65d8add0fddaeeb7f
Bashamega <adambashaahmednaji@gmail.com>
2024-04-09 06:50:13 +0200

    Palestine update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<main className="h-[100vh] w-[100vw] flex items-center justify-center bg-[#527853] overflow-hidden wrapper">
+              
              <div className='dot-container hidden'><span className="dot"></span></div>
              <div className='dot-container'><span className="dot">أفعال</span></div>
              <div className='dot-container'><span className="dot">حروف</span></div>
@@ -17,10 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
              <div className='dot-container'><span className="dot">النواسخ</span></div>
              <div className='dot-container'><span className="dot">النعت</span></div>
              <div className='dot-container'><span className="dot">الحال</span></div>
-             
+             <div  className="absolute top-0 right-0 bg-black text-red-500 text-2xl font-bold origin-top float-right mt-9 mr-9 w-72 text-center" style={{transform:"translateX(50%) rotate(45deg)"}} >
+              <div>Free <br></br> Palestine</div>
+            </div>
             <div className=" bg-[#F9E8D9] p-10 rounded-lg shadow-lg text-center space-y-3 " style={{zIndex: 1000}}>
               <Component {...pageProps} />
             </div>
+            
             <footer className="bg-gray-200 p-4 text-center fixed bottom-0 w-screen">
               <p className="text-sm text-gray-600">
                 Created with{' '}
