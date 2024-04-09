7fc11d03a4233575437d997720df2bf695fb0845
Bashamega <adambashaahmednaji@gmail.com>
2024-04-09 11:20:57 +0200

    update

---
+++ src/pages/2/2/2/index.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Link from "next/link";
+export default function lessonComponent(){
+    return(
+        <div className="lg:w-[700px]">
+            <Link href={"/2/2"}>
+                <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                    d="M15 18L9 12L15 6"
+                    stroke="black"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    />
+                </svg>
+                </Link>
+                <h1 className="text-7xl">المفعول لأجله</h1>
+                <br></br>
+                <p className="text-[#527853] text-lg">يُعرّف المفعول لأجله أو ما يسمّى بالمفعول له عند أهل الّلغة أنه المصدرُ المنصوب الذي يُعلِّلُ حدثًا مُعينًا مع مُشاركته لزمانه وفاعلِه</p>
+                <br />
+                <div className="text-[#527853] text-lg"dir="rtl">
+                    <h1 className="text-2xl">الإعراب:</h1>
+                    <p>المفعول لأجله منصوب دائمُا وعلاماته:</p>
+                    <div className=" lg:grid lg:grid-cols-3 mb-5 lg:gap-5 text-lg mt-10">
+                        <div className="group mx-auto">
+                            <p className="text-orange-500 cursor-pointer">الفتحة</p>
+                            <div className="p-1 bg-orange-500 rounded hidden group-hover:flex absolute shadow-lg w-[150px] h-[100px] justify-center items-center">
+                            <svg className="absolute text-orange-500 h-2 mr-5  right-0 bottom-full rotate-180" x="0px" y="0px" viewBox="0 0 255 255" ><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
+                            <ul className="text-[#fff]">
+                                <li>المفرد</li>
+                                <li>جمع التكسير</li>
+                            </ul>
+                            </div>
+                        </div>
+                        <div className="group mx-auto">
+                            <p className="text-orange-500 cursor-pointer">الياء</p>
+                            <div className="p-1 bg-orange-500 rounded hidden group-hover:flex absolute shadow-lg w-[150px] h-[100px] justify-center items-center">
+                            <svg className="absolute text-orange-500 h-2 mr-5  right-0 bottom-full rotate-180" x="0px" y="0px" viewBox="0 0 255 255" ><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
+
+                            <ul className="text-[#fff]">
+                                <li>المثنى</li>
+                                <li>جمع المذكر السالم</li>
+                                <li>أسماء الخمسة</li>
+                            </ul>
+                            </div>
+                        </div>
+                        <div className="group mx-auto">
+                            <p className="text-orange-500 cursor-pointer">الكسرة</p>
+                            <div className="p-1 bg-orange-500 rounded hidden group-hover:flex absolute shadow-lg w-[150px] h-[100px] justify-center items-center">
+                            <svg className="absolute text-orange-500 h-2 mr-5  right-0 bottom-full rotate-180" x="0px" y="0px" viewBox="0 0 255 255" ><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
+                            <ul className="text-[#fff]">
+                                <li>جمع المؤنث السالم</li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="text-[#527853] text-lg text-right"dir="rtl">
+                    
+                    <h1 className="text-2xl">أمثلة:</h1>
+                    <ul className="text-[#527853]" style={{ listStyleType: 'arabic-indic' }}>
+                        <li>
+                             استبسل الأبطار <span className="underline text-orange-500 group cursor-pointer relative">
+                                 دافعًا  
+                                <div className=" whitespace-nowrap white  opacity-0 w-auto overflow-visible bg-[#dae0da] text-lg shadow-2xl text-center rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full ml-14 px-3 pointer-events-none">
+                                مفعول لأجله منصوب بالفتحة الظاهرة في آخره  
+                                </div>
+                                </span> عن وطنهم
+                             </li>
+                        <li>زيد مسرع  <span className="underline text-orange-500 relative group cursor-pointer">جوفًا
+                        <div className=" whitespace-nowrap white  opacity-0 w-auto overflow-visible bg-[#dae0da] text-lg shadow-2xl text-center rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full ml-14 px-3 pointer-events-none">
+                        مفعول لأجله منصوب بالفتحة الظاهرة في آخره .   
+                                </div>
+                        </span> من الكلب</li>
+                        <li>قمت  <span className="underline text-orange-500 relative group cursor-pointer">إجلالًا <div className=" whitespace-nowrap white  opacity-0 w-auto overflow-visible bg-[#dae0da] text-lg shadow-2xl text-center rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full ml-14 px-3 pointer-events-none">
+                        مفعول لأجله منصوب بالفتحة الظاهرة في آخره 
+                        </div> </span> لأستاذي</li>
+                        <li>يجتهد زيد <span className="underline text-orange-500 group relative cursor-pointer">طلبَ<div className=" whitespace-nowrap white  opacity-0 w-auto overflow-visible bg-[#dae0da] text-lg shadow-2xl text-center rounded-lg py-2 absolute z-10 group-hover:opacity-100 bottom-full ml-14 px-3 pointer-events-none">
+                        مفعول لأجله منصوب بالفتحة الظاهرة في آخره وهو مضاف 
+                            </div> </span> التفوق</li>
+                        
+                    </ul>
+                </div>
+
+        </div>
+    )
+}
